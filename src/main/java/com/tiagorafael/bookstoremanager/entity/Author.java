e43d928606b55e7f14171311b3c502f8bf318ada
Tiago Rafael Oliveira das Virgens <tiago.rafael@compasso.com.br>
2023-12-06 15:41:54 -0400

    feat: add entity

---
+++ src/main/java/com/tiagorafael/bookstoremanager/entity/Author.java
@@ -0,0 +1,25 @@
+package com.tiagorafael.bookstoremanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
